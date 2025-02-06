44147ecc66a3544809ab23980bf137e8d1442485
Ariel <94843124+ArielCNunes@users.noreply.github.com>
2025-02-06 15:19:37 +0000

    Setting up routes

--- ai-hub/frontend/ai-hub-ui/src/app/app.routes.ts
+++ ai-hub/frontend/ai-hub-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'chat', component: ChatComponent }
+];
