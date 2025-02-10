d2967301b31c9bc40250b80f4d3423c3bfb296e1
Ariel <94843124+ArielCNunes@users.noreply.github.com>
2025-02-10 22:49:28 +0000

    Working on fixing Ionic bug

--- ai-hub/frontend/ai-hub-ui/src/app/components/home/home.component.ts
+++ ai-hub/frontend/ai-hub-ui/src/app/components/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonButton, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  standalone: true,
   imports: [
-    IonicModule,   // Import IonicModule to use Ionic components
+    IonButton, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardHeader
   ]
 })
 export class HomeComponent { }
