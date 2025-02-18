6f23b07a89bc8a8bbedc605cae06e2be78dc73a9
Ariel <94843124+ArielCNunes@users.noreply.github.com>
2025-02-18 18:40:43 +0000

    Adding my credentials

--- ai-hub/frontend/ai-hub-ui/src/app/environments/environment.ts
+++ ai-hub/frontend/ai-hub-ui/src/app/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id"
-  }
-};
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyBaFB0UWsFLpvyvV4McO2UdehNVDJBnTAA",
+      authDomain: "ai-hub-a61e1.firebaseapp.com",
+      projectId: "ai-hub-a61e1",
+      storageBucket: "ai-hub-a61e1.appspot.com",
+      messagingSenderId: "547282561512",
+      appId: "1:547282561512:web:e0dec28b40bb17e0951c5d",
+      measurementId: "G-JY12MRGYR6"
+    }
+  };
