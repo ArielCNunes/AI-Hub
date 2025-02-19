b30b1b22341b6c25504466b7fee49dd271b84829
Ariel <94843124+ArielCNunes@users.noreply.github.com>
2025-02-19 11:16:39 +0000

    Adding API key

--- ai-hub/frontend/ai-hub-ui/src/app/environments/environment.ts
+++ ai-hub/frontend/ai-hub-ui/src/app/environments/environment.ts
@@ -9,5 +9,5 @@ export const environment = {
       appId: "1:547282561512:web:e0dec28b40bb17e0951c5d",
       measurementId: "G-JY12MRGYR6"
     },
-    openaiApiKey: 'your-api-key-here'
+    openaiApiKey: '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'
   };
