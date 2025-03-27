182536b9434e65e1bff0d66f8c63cc9156f2f756
Ariel <94843124+ArielCNunes@users.noreply.github.com>
2025-03-27 23:06:17 +0000

    Add Android project files and configuration for Capacitor integration

---
+++ ai-hub/frontend/ai-hub-ui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ariel.aihub',
+  appName: 'AI-Hub',
+  webDir: 'dist/ai-hub-ui/browser'
+};
+
+export default config;
